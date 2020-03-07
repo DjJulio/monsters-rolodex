aae3937fccb5dc1f91918a9904c17bd52296f36a
julio Navarro <julionavarro@la-MacBook-Pro-de-julio.local>
2020-03-07 00:00:33 -0600

    Updates

---
+++ precache-manifest.4843188eaf1c931413caf0775eb316b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9cb13234e93865ddafe02a857cff25",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c25d50977ea853bb6ab1",
+    "url": "/monsters-rolodex/static/css/main.25eb91e7.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c25d50977ea853bb6ab1",
+    "url": "/monsters-rolodex/static/js/main.80fe7127.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
